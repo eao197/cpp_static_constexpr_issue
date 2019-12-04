5a4783000d526c285004ffbba1472a3815cdcab3
Yauheni Akhotnikau <eao197@gmail.com>
2019-12-04 10:18:59 +0300

    A working workaround based on enum class.

--- qvalue.hpp
+++ qvalue.hpp
@@ -10,6 +10,9 @@ using underlying_uint_t = std::uint_least16_t;
 constexpr underlying_uint_t maximum = 1000u;
 constexpr underlying_uint_t zero = 0u;
 
+enum class extremum_min_t { minimum };
+enum class extremum_max_t { maximum };
+
 class trusted
 {
 	const underlying_uint_t m_value;
@@ -29,8 +32,11 @@ public :
 	using underlying_uint_t = qvalue_details::underlying_uint_t;
 	using trusted = qvalue_details::trusted;
 
-	static constexpr trusted maximum{ qvalue_details::maximum };
-	static constexpr trusted zero{ qvalue_details::zero };
+	static constexpr qvalue_details::extremum_max_t maximum
+			= qvalue_details::extremum_max_t::maximum;
+
+	static constexpr qvalue_details::extremum_min_t zero
+			= qvalue_details::extremum_min_t::minimum;
 
 private :
 	underlying_uint_t m_value{};
@@ -41,6 +47,14 @@ public :
 
 	constexpr qvalue_t( trusted val ) noexcept
 		:	m_value{ val.get() }
+	{}
+
+	constexpr qvalue_t( qvalue_details::extremum_max_t ) noexcept
+		:	m_value{ qvalue_details::maximum }
+	{}
+
+	constexpr qvalue_t( qvalue_details::extremum_min_t ) noexcept
+		:	m_value{ qvalue_details::zero }
 	{}
 
 	constexpr auto as_uint() const noexcept { return m_value; }
